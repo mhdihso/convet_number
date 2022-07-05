cafe31f9b3014331ef084330d199251b6f1ec899
mahdi <mahdihos38@gmail.com>
2022-07-05 21:26:40 +0430

    bugs fixed

--- convert/tartibi.py
+++ convert/tartibi.py
@@ -57,88 +57,93 @@ data = {
 
 and_word = "و"
 
+def numbers_less_than_100(number):
+    number = str(number)
+    if number == "1":
+        return data["1f"]
+    elif number == "2":
+        return data["2"]
+    elif number == "3":
+        return data["3"]
+    elif number == "4":
+        return data["4"]
+    elif number == "5":
+        return data["5"]
+    elif number == "6":
+        return data["6"]
+    elif number == "7":
+        return data["7"]
+    elif number == "8":
+        return data["8"]
+    elif number == "9":
+        return data["9"]
+    elif number == "10":
+        return data["10"]
+    elif number == "11":
+        return data["11"]
+    elif number == "12":
+        return data["12"]
+    elif number == "13":
+        return data["13"]
+    elif number == "14":
+        return data["14"]
+    elif number == "15":
+        return data["15"]
+    elif number == "16":
+        return data["16"]
+    elif number == "17":
+        return data["17"]
+    elif number == "18":
+        return data["18"]
+    elif number == "19":
+        return data["19"]
+    elif number == "20":
+        return data["20"]
+    elif number == "30":
+        return data["30"]
+    elif number == "40":
+        return data["40"]
+    elif number == "50":
+        return data["50"]
+    elif number == "60":
+        return data["60"]
+    elif number == "70":
+        return data["70"]
+    elif number == "80":
+        return data["80"]
+    elif number == "90":
+        return data["90"]
+    else:
+        number = int(number)
+        mod = number % 10 
+        return data[f"{number - mod}f"] + and_word + data[str(number % 10)]
+
+
 class Convert:
     def __init__(self, number):
         self.number = number
 
-    def numbers_less_than_100(self,number):
-        number = str(number)
-        if number == "1":
-            return data["1f"]
-        elif number == "2":
-            return data["2"]
-        elif number == "3":
-            return data["3"]
-        elif number == "4":
-            return data["4"]
-        elif number == "5":
-            return data["5"]
-        elif number == "6":
-            return data["6"]
-        elif number == "7":
-            return data["7"]
-        elif number == "8":
-            return data["8"]
-        elif number == "9":
-            return data["9"]
-        elif number == "10":
-            return data["10"]
-        elif number == "11":
-            return data["11"]
-        elif number == "12":
-            return data["12"]
-        elif number == "13":
-            return data["13"]
-        elif number == "14":
-            return data["14"]
-        elif number == "15":
-            return data["15"]
-        elif number == "16":
-            return data["16"]
-        elif number == "17":
-            return data["17"]
-        elif number == "18":
-            return data["18"]
-        elif number == "19":
-            return data["19"]
-        elif number == "20":
-            return data["20"]
-        elif number == "30":
-            return data["30"]
-        elif number == "40":
-            return data["40"]
-        elif number == "50":
-            return data["50"]
-        elif number == "60":
-            return data["60"]
-        elif number == "70":
-            return data["70"]
-        elif number == "80":
-            return data["80"]
-        elif number == "90":
-            return data["90"]
-        else:
-            number = int(number)
-            mod = number % 10 
-            return data[f"{number - mod}f"] + and_word + data[str(number % 10)]
 
         
     def convert_number(self):
         str_number = str()
         try:
-            number = str(self.number)
+            number = str(self)
             str_number = data[number]
             return str_number
         except KeyError:
-            if self.number <100:
-                number = self.numbers_less_than_100(self.number)
+            if self <100:
+                number = numbers_less_than_100(int(number))
                 return number
             else:
-                if self.number%100 == 1:
+                if self%100 == 1:
                     number = data["1"]
                 else:
-                    number = self.numbers_less_than_100(self.number%100)
-                bigger_number = data[str(self.number - self.number%100)]
+                    n = int(number)%100
+                    number_ = numbers_less_than_100(int(n))
+                n = int(number)%100
+                bigger_number = data[str(int(number) - n)]
 
-                return f"{bigger_number} {and_word} {number}"
+                return f"{bigger_number} {and_word} {number_}"
+
 
