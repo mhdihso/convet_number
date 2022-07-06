e161685bef7ac7093170a033b9ec09cf95af3f4f
mehdi hoseyni <70570674+mhdihso@users.noreply.github.com>
2022-07-06 15:47:50 +0430

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='convert',
-    version='0.1.1',    
+    version='0.1.2',    
     description='A example Python package',
     url='https://github.com/shuds13/pyexample',
     author='Stephen Hudson',
